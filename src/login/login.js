f089afa31d558fa37e2b374df0a5af1e3f8b58c8
IamFayaz97 <fayazdreamer@gmail.com>
2021-02-11 22:36:08 +0530

    login

--- src/login/login.js
+++ src/login/login.js
@@ -1,1 +1,10 @@
-var firstName = document.getElementsByName("firstName");
+function changeUserName(){
+
+    var formData = new FormData(document.querySelector('form'));
+    localStorage.setItem("loggedUserName", formData.get("firstName") + " " + formData.get("lastName"));
+    document.getElementById("loggedUserName").innerHTML = localStorage.getItem("loggedUserName");
+};
+// document.getElementById("loggedUserName").innerHTML = localstorage.getItem('loggedUserName');
+var userName = localStorage.getItem("loggedUserName");
+
+document.getElementById("loggedUserName").innerHTML = userName;
