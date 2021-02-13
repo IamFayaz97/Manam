6f34ba8e77273932a98c697900d9615f80b539be
IamFayaz97 <fayazdreamer@gmail.com>
2021-02-14 01:26:42 +0530

    modify quantity operations

--- src/login/login.js
+++ src/login/login.js
@@ -3,26 +3,43 @@ function changeUserName() {
     localStorage.setItem("loggedUserName", formData.get("firstName") + " " + formData.get("lastName"));
     document.getElementById("loggedUserName").innerHTML = localStorage.getItem("loggedUserName");
 };
-// document.getElementById("loggedUserName").innerHTML = localstorage.getItem('loggedUserName');
 var userName = localStorage.getItem("loggedUserName");
 document.getElementById("loggedUserName").innerHTML = userName;
 
 
 // quantity incrementer
 function modifyQuantity(id, operation) {
-    console.log(localStorage.getItem(id) + "see");
-    if (localStorage.getItem(id) > 0) {
+    var quantity = Number(localStorage.getItem(id));
         if (operation == 'increment') {
-            localStorage.setItem(id, Number(localStorage.getItem(id)) + 1);
+            localStorage.setItem(id, quantity + 1);//increment the quantity
         } else {
-            localStorage.setItem(id, Number(localStorage.getItem(id)) - 1);
+            (quantity == 1) ? showAddButton(id) : localStorage.setItem(id, quantity - 1); //check and decrement the quantity
         }
-    } else {
-        (operation == 'increment') ? localStorage.setItem(id, 1) : localStorage.setItem(id, 0);
-    }
-    if (localStorage.getItem(id) > 0) {
-        document.getElementById(id).innerHTML = '( ' + localStorage.getItem(id) + " )";
-    } else {
-        document.getElementById(id).innerHTML = '';
-    }
+
+    document.getElementById(id).innerHTML = '( ' + localStorage.getItem(id) + " )";
+
 }
+
+//hide add button and show modify buttons
+function hideAddButton(id) {
+    var addButton = document.getElementById('addButton_' + id);
+    var modifyButtons = document.getElementById('modifyButtons_' + id);
+    addButton.style.display = "none"; //hiding Add button
+    localStorage.setItem(id, 1);
+    document.getElementById(id).innerHTML = '( ' + localStorage.getItem(id) + " )";
+    modifyButtons.style.display = "block"; //showing increment and decrement buttons
+}
+
+//show add button and hide modify buttons
+function showAddButton(id){
+    document.getElementById('addButton_' + id).style.display = "block";
+    document.getElementById('modifyButtons_' + id).style.display = "none";
+    localStorage.setItem(id, 0);
+}
+
+var lunch_ChickenBiryani = 200;
+var lunch_EggBiryani = 150;
+var lunch_FullVegMeal = 100;
+var lunch_CurdRice = 50;
+var lunch_VegPulaav = 75;
+var lunch_Tandoori = 100;
